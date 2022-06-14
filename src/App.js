d481336ba5ccb31a3476326efababa535e091ed4
charliebrown1221 <chaimbrown1@yahoo.com>
2022-06-14 11:15:05 -0400

    created AddEntryForm file and folder + instatiated it +added button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
-
+import AddEntryForm from './Components/AddEntries/AddEntryForm';
 function App() {
 
  const[entries, setEntries ]=useState([{weight: 175, date: '11-23-2021'}])
+
+ function addNewEntry(entry){
+  let tempEntry =[ ...entries, entry];
+
+  setEntries(tempEntry);
+ }
 
   return (
     <div>
     
    <DisplayEntries parentEntries={entries}/>
+  <AddEntryForm addNewEntryProperty={addNewEntry}/>
 
     </div>
   );
